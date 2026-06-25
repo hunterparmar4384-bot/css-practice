1a08998236cf69833195f5ac1fbb2b457b5bc9c4
shaktisinh <hunterparmar4384@gmail.com>
2026-06-25 19:09:09 +0530

    states

---
+++ React/bootstrap/src/Bootstrap.jsx
@@ -0,0 +1,29 @@
+import Button from 'react-bootstrap/Button';
+function TypesExample() {
+  return (
+    <>
+    <div className='first'>
+      <Button variant="primary">Primary</Button>
+      <Button variant="secondary">Secondary</Button>
+      <Button variant="success">Success</Button>
+      <Button variant="warning">Warning</Button>
+      <Button variant="danger">Danger</Button>
+      <Button variant="info">Info</Button>
+      <Button variant="light">Light</Button>
+      <Button variant="dark">Dark</Button>
+      <Button variant="link">Link</Button>
+    </div>
+    <br/><br/>
+    <Button variant="outline-primary">Primary</Button>
+      <Button variant="outline-secondary">Secondary</Button>
+      <Button variant="outline-success">Success</Button>
+      <Button variant="outline-warning">Warning</Button>
+      <Button variant="outline-danger">Danger</Button>
+      <Button variant="outline-info">Info</Button>
+      <Button variant="outline-light">Light</Button>
+      <Button variant="outline-dark">Dark</Button>
+    </>
+  );
+}
+
+export default TypesExample;
